1d1bc5687a62f4ac4ab9f2bcd38157e2952bd156
Eric Dell'Oro <eric.delloro@gmail.com>
2016-06-09 12:03:10 +1000

    CODE UPDATES 2016

---
+++ CONSUMERS AND SERVICES/WEBFORM-SPA-ANGULAR-WEBSERVICE/App_Code/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using MoonAndBackCalculatorApplication.ConstantEngine;
+using MoonAndBackCalculatorApplication.Model;
+
+namespace MoonAndBackCalculatorApplication.Controllers
+{
+
+    /// <summary>
+    /// Summary description for Controller
+    /// </summary>
+    [WebService(Namespace = "http://JenniferNine.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.Web.Script.Services.ScriptService]
+    public class Controller : System.Web.Services.WebService
+    {
+        public Controller()
+        {
+            //Uncomment the following line if using designed components 
+            //InitializeComponent(); 
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public CalculatorResult Calculate()
+        {
+            constantModel model = new constantModel();
+            IconstantEngine engine = null;
+            engine = model.GetconstantEngine();
+            CalculatorResult rc = null;
+            rc = engine.Calculate();
+            return new CalculatorResult(rc.PI, rc.CalculationTime);
+        }
+    }
+}
---
+++ CONSUMERS AND SERVICES/WEBFORM-SPA-ANGULAR-WEBSERVICE/App_Code/Model.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoonAndBackCalculatorApplication.Model
+{
+   // MODEL IS A PROJECT REFERENCE COMMON TO ALL
+}
